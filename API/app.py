7936cda466847962c4c557fe79f4514cade87dbf
Wytamma Wirth <wytamma.wirth@me.com>
2017-08-20 16:17:55 +1000

    clean

--- API/app.py
+++ API/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from pymongo import MongoClient
 from flask_restful import Resource, reqparse, Api
 from _passwords import MONGO_URI
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
