b1de27baa17925f05e24964578ba47e4623c0672
Wytamma Wirth <wytamma.wirth@me.com>
2017-11-16 11:22:06 +1000

    restrict probes to a single pi
    each pi was looking for every probe...

--- RPi/TankTemp.py
+++ RPi/TankTemp.py
@@ -83,7 +83,8 @@ records = []  # stores temperture records in memory before sending them to DB
 while True:
     # get fresh probe info
     try:
-        r = requests.get(API_BASE_URL + '/probes')
+        hostname = os.uname()[1]
+        r = requests.get(API_BASE_URL + '/probes?hostname=' + hostname)
         if r.status_code == 200:
             probesInfoFromAPI = r.json()['data']
         else:
